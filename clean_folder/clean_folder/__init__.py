68386999eadda23cd11349f04eb91c1d85781882
DenysTantiura <111456850+DenysTantsiura@users.noreply.github.com>
2022-09-16 13:09:06 +0300

    something corected

--- clean_folder/clean_folder/__init__.py
+++ clean_folder/clean_folder/__init__.py
@@ -1,5 +1,6 @@
 from clean_folder.type_extensions import data_base_of_extensions, main_directories
 from clean_folder.norma import normalization_scheme, normalize
+# this (*) is bad practice, just for example here:
 from clean_folder.variables import *
 
 __all__ = ['data_base_of_extensions', 'main_directories',
